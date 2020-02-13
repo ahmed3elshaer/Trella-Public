8cc53d3b7a13d6541a3456fbcdef81f89851609b
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2020-02-13 02:54:05 +0200

    adding generic schedulers handling

---
+++ app/src/main/java/com/trella/common/base/baseUseCase.kt
@@ -0,0 +1,30 @@
+package com.trella.common.base
+
+import com.trella.common.schedulers.BaseSchedulerProvider
+import io.reactivex.CompletableTransformer
+import io.reactivex.ObservableTransformer
+import io.reactivex.SingleTransformer
+import javax.inject.Inject
+
+abstract class BaseUseCase(private val baseSchedulerProvider: BaseSchedulerProvider) {
+    fun <X> applySchedulers(): ObservableTransformer<X, X> {
+        return ObservableTransformer { up ->
+            up.subscribeOn(baseSchedulerProvider.io())
+                .observeOn(baseSchedulerProvider.ui())
+        }
+    }
+
+    fun <X> applySchedulersSingle(): SingleTransformer<X, X> {
+        return SingleTransformer { up ->
+            up.subscribeOn(baseSchedulerProvider.io())
+                .observeOn(baseSchedulerProvider.ui())
+        }
+    }
+
+    fun applySchedulersCompletable(): CompletableTransformer {
+        return CompletableTransformer { up ->
+            up.subscribeOn(baseSchedulerProvider.io())
+                .observeOn(baseSchedulerProvider.ui())
+        }
+    }
+}
